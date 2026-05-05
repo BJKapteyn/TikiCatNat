f7a24f5c03fc0a20b2275fd8fe1ffacf7eb1d4bf
Brad K <bjkapteyn@gmail.com>
2026-05-05 18:55:33 -0400

    added empty component for home page head

---
+++ src/assets/Components/HomePage/HomePageHeader/HomepageHead.tsx
@@ -0,0 +1,12 @@
+
+import './HomepageHeader.css';
+
+const HomepageHeader: React.FC = () => {
+    return (
+        <header className="homepageHeader">
+            {/* empty component */}
+        </header>
+    );
+};
+
+export default HomepageHeader;

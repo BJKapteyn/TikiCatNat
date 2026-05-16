f232f3f31006b5da8196fb950ba11ab1359a1f7c
Brad K <bjkapteyn@gmail.com>
2026-05-16 13:18:16 -0400

    added react spring, added cat photos, added empty photo viewer

---
+++ src/assets/Components/PhotoViewer/PhotoViewer.tsx
@@ -0,0 +1,13 @@
+import './PhotoViewer.css';
+
+const PhotoViewer: React.FC = () => {
+    return (
+        <section className="photo-viewer">
+            <div className="photo-viewer-placeholder">
+                Photo Viewer Component
+            </div>
+        </section>
+    );
+};
+
+export default PhotoViewer;

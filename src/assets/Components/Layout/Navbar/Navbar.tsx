d9ce03ea0afe4f446ff868f0a4ed97e49a1eee4e
Brad K <bjkapteyn@gmail.com>
2026-08-04 22:23:34 -0400

    enhance Navbar component to track visibility state with useInView

--- src/assets/Components/Layout/Navbar/Navbar.tsx
+++ src/assets/Components/Layout/Navbar/Navbar.tsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router";
 import { NavBarLinks } from "./NavBarLinks";
 import { useInView } from "react-intersection-observer";
+import { useState } from "react";
 import logo from "../../../Images/Logos/LOGO-V1-transparent.png";
 import "./Navbar.css";
 
 const Navbar = () => {
+    const [isInView, setIsInView] = useState(true);
     const { ref, inView } = useInView({
         threshold: 0.1,
+        initialInView: true,
     })
 
-    if(!inView) {
-        console.log("in view");
+    if(!inView && isInView) {
+        setIsInView(false);
+        console.log("state changed to false");
+    } else if(inView && !isInView) {
+        setIsInView(true);
+        console.log("state changed to true");
     }
 
     return (
